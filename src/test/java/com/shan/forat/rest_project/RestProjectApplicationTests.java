eb8ad32a96c92a9bdac0bed00505006448efc317
hasan foraty <hasanforaty@gmail.com>
2023-09-05 17:03:52 +0330

    git init

---
+++ src/test/java/com/shan/forat/rest_project/RestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shan.forat.rest_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
